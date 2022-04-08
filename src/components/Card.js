f52b7e4786471c1de502f456ffeab4b533b9105b
Evgeny Synkov <synkoveugeny@gmail.com>
2022-04-08 17:43:15 +0300

    Сделан фикс карточки и картинок в других карточках

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
-function Card({id, title, price, imagePath, slide}) {
+function Card({ id, title, price, imagePath, slide }) {
     return (
-        <div id={id} className={slide ? "card_theme_slide card" : "card"}>
-            <img className="card__image" src={imagePath} alt="Heathens picture" />
-            <h3 className="card__title">{title}</h3>
-            <p className="card__price">{price}$</p>
-        </div>
+      <div id={id} className={slide ? "card_theme_slide card" : "card"}>
+        <img className="card__image" src={imagePath} alt="Heathens picture" />
+        <h3 className="card__title">{title}</h3>
+        <p className="card__price">{price}$</p>
+      </div>
     );
   }
   
